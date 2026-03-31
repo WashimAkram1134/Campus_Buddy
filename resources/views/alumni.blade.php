db6c64c5d5213872f545a7550e2b684848b10db4
mayazad <officialmayazad@gmail.com>
2026-03-31 08:38:47 +0600

    updated the Alumni card designs to use the Company Logo

--- resources/views/alumni.blade.php
+++ resources/views/alumni.blade.php
@@ -130,8 +130,10 @@
     <div class="alumni-grid reveal">
         @foreach($approvedAlumni as $alumni)
         <div class="alumni-card featured-card reveal animate-item up" data-category="{{ $alumni->category }}">
-            <div class="card-top">
-                @if($alumni->card_bg_image)
+            <div class="card-top" style="overflow: hidden;">
+                @if($alumni->company_logo)
+                    <img src="{{ asset('storage/' . $alumni->company_logo) }}" alt="{{ $alumni->company }}" class="field-img" style="width: 100%; height: 100%; object-fit: cover;">
+                @elseif($alumni->card_bg_image)
                     <img src="{{ asset('storage/' . $alumni->card_bg_image) }}" alt="{{ $alumni->company }}" class="field-img">
                 @else
                     <img src="{{ asset('images/alumni/alumni_tech_bg.png') }}" alt="{{ $alumni->company }}" class="field-img">
